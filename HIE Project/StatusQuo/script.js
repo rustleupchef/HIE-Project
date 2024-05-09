3ce3bfb7aeac5a535475a5b5b4fdf19bf54f3dde
rustleupchef <123332908+rustleupchef@users.noreply.github.com>
2024-05-09 07:16:48 -0500

    Add files via upload

---
+++ HIE Project/StatusQuo/script.js
@@ -0,0 +1,31 @@
+const origin = "C:/Users/ravin/source/repos/HIE%20Project/";
+const array = ["test.html", "dog.html"];
+let point = 0;
+
+function StatusQuo() {
+    window.location.pathname = origin + "StatusQuo.html";
+}
+
+function NuclearEnergy() {
+    window.location.pathname = origin + "NuclearEnergy.html"
+}
+
+function Influence() {
+    window.location.pathname = origin + "Influence.html";
+}
+
+function Forward() {
+    point++;
+    if (point == array.length) {
+        point -= 1;
+    }
+    document.getElementById("content").setAttribute("src", "context/" + array[point]);
+}
+
+function Backward() {
+    point--;
+    if (point < 0) {
+        point = 0;
+    }
+    document.getElementById("content").setAttribute("src", "context/" + array[point]);
+}
