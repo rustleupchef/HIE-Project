517d1845b817065ea70604abafd95a5de35b9726
rustleupchef <123332908+rustleupchef@users.noreply.github.com>
2024-05-12 21:37:02 -0500

    Add files via upload

---
+++ HIE Project/Influence/script.js
@@ -0,0 +1,16 @@
+const origin = "C:/Users/ravin/source/repos/HIE%20Project/";
+function StatusQuo() {
+    window.location.pathname = origin + "StatusQuo.html";
+}
+
+function NuclearEnergy() {
+    window.location.pathname = origin + "NuclearEnergy.html"
+}
+
+function Influence() {
+    window.location.pathname = origin + "Influence.html";
+}
+
+function Submit() {
+    
+}
