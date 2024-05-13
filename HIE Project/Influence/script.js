af81f6897b1e9cb5f4803c212ef31bedb90f5cd1
rustleupchef <123332908+rustleupchef@users.noreply.github.com>
2024-05-13 10:17:36 -0500

    Update script.js

--- HIE Project/Influence/script.js
+++ HIE Project/Influence/script.js
@@ -1,4 +1,7 @@
 const origin = "HIE-Project/HIE%20Project/";
+const array = [];
+let point = -1;
+
 function StatusQuo() {
     window.location.pathname = origin + "StatusQuo.html";
 }
@@ -11,6 +14,18 @@ function Influence() {
     window.location.pathname = origin + "Influence.html";
 }
 
-function Submit() {
-    
+function Forward() {
+    point++;
+    if (point == array.length) {
+        point -= 1;
+    }
+    document.getElementById("content").setAttribute("src", "context/" + array[point]);
+}
+
+function Backward() {
+    point--;
+    if (point < 0) {
+        point = 0;
+    }
+    document.getElementById("content").setAttribute("src", "context/" + array[point]);
 }
