f40f34ce86fb025986ec0a3e979b92a6ce9d7dcc
rustleupchef <123332908+rustleupchef@users.noreply.github.com>
2024-05-17 22:33:02 -0500

    Add files via upload

---
+++ Influence/script.js
@@ -0,0 +1,23 @@
+const origin = "C:/Users/ravin/source/repos/HIE Project/";
+const array = ["Quiz/Quiz.html"];
+let point = 0;
+
+window.onload = function() {
+    document.getElementById("content").setAttribute("src", "context/" + array[0]);
+}
+
+function StatusQuo() {
+    window.location.pathname = origin + "StatusQuo.html";
+}
+
+function NuclearEnergy() {
+    window.location.pathname = origin + "NuclearEnergy.html"
+}
+
+function Influence() {
+    window.location.pathname = origin + "Influence.html";
+}
+
+function Submit() {
+    
+}
